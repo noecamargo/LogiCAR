d2634409e8242c363b4476869f385d997d2d7c54
acapece <acapece@velocitypartners.net>
2017-10-04 13:06:18 -0300

    Creacion entidades inspeccion Test

---
+++ LogiCAR.Entidades.Test/InspeccionTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogiCAR.Entidades.Test
+{
+    [TestClass]
+    public class InspeccionTest
+    {
+        [TestMethod]
+        public void CrearInspeccion()
+        {
+            Inspeccion inspeccion = new Inspeccion();
+           
+        }
+    }
+}
