c99177fcfa37e242c321111343b99c966b14cdc3
acapece <acapece@velocitypartners.net>
2017-10-05 21:05:59 -0300

    Creacion pruebas Danio en EntidadesTest

---
+++ LogiCAR.Entidades.Test/DanioTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LogiCAR.Entidades;
+using System.Drawing;
+
+namespace LogiCAR.Entidades.Test
+{
+    [TestClass]
+    public class DanioTest
+    {
+        [TestMethod]
+        public void CrearDanio()
+        {
+            Danio danio = new Danio();
+            danio.Id = 1;
+            danio.Descripcion = "Parabrisas roto";
+            danio.Foto = new byte[0];
+
+            Assert.IsNotNull(danio);
+            Assert.IsNotNull(danio.Id);
+            Assert.IsNotNull(danio.Descripcion);
+            Assert.IsNotNull(danio.Foto);
+        }
+    }
+}
