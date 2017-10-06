b1b3524d27935a6ef5717483a41e9e5435ed7b33
acapece <acapece@velocitypartners.net>
2017-10-05 23:36:06 -0300

    Creacion pruebas Lote EntidadTest, clase Lote y Controller Lote

--- Logi.CAR.WebApi.Tests/LoteControllerTest.cs
+++ Logi.CAR.WebApi.Tests/LoteControllerTest.cs
@@ -40,5 +40,112 @@ namespace Logi.CAR.WebApi.Tests
 
 
         }
+
+        [TestMethod]
+        public void ObtenerLote()
+        {
+            //Arrange: Construimos el mock y seteamos las expectativas
+            var lote = GenerarLote();
+            var mockLoteLogica = new Mock<ILogicaNegocioLote>();
+            mockLoteLogica
+                .Setup(bl => bl.ObtenerLote(lote.Id))
+                .Returns(lote);
+
+            var controller = new LoteController(mockLoteLogica.Object);
+
+            //Act: Efectuamos la llamada al controller
+            IHttpActionResult resultadoObtenido = controller.Get(lote.Id);
+
+            //Assert
+            var contentResult = resultadoObtenido as OkNegotiatedContentResult<Lote>;
+            mockLoteLogica.VerifyAll();
+            Assert.IsNotNull(contentResult);
+            Assert.IsNotNull(contentResult.Content);
+            Assert.AreEqual(lote, contentResult.Content);
+        }
+
+        [TestMethod]
+        public void ObtenerLotees()
+        {
+            //Arrange: Construimos el mock y seteamos las expectativas
+            var lotees = GenerarLotees();
+            var mockLoteesLogica = new Mock<ILogicaNegocioLote>();
+            mockLoteesLogica
+                .Setup(bl => bl.ObtenerLotees())
+                .Returns(lotees);
+
+            var controller = new LoteController(mockLoteesLogica.Object);
+
+            //Act: Efectuamos la llamada al controller
+            IHttpActionResult resultadoObtenido = controller.Get();
+
+            //Assert
+            var contentResult = resultadoObtenido as OkNegotiatedContentResult<IEnumerable<Lote>>;
+            mockLoteesLogica.VerifyAll();
+            Assert.IsNotNull(contentResult);
+            Assert.IsNotNull(contentResult.Content);
+            Assert.AreEqual(lotees, contentResult.Content);
+        }
+
+        [TestMethod]
+        public void ModificarLote()
+        {
+            //Arrange: Construimos el mock y seteamos las expectativas
+            var lote = GenerarLote();
+            var mockLoteesLogica = new Mock<ILogicaNegocioLote>();
+            mockLoteesLogica
+                .Setup(bl => bl.ActualizarLote(lote.Id, lote))
+                .Returns(true);
+
+            var controller = new LoteController(mockLoteesLogica.Object);
+
+            //Act: Efectuamos la llamada al controller
+            IHttpActionResult obtainedResult = controller.Put(lote.Id, lote);
+
+            //Assert
+            var createdResult = obtainedResult as CreatedAtRouteNegotiatedContentResult<Lote>;
+            mockLoteesLogica.VerifyAll();
+            Assert.IsNotNull(createdResult);
+            Assert.AreEqual("DefaultApi", createdResult.RouteName);
+            Assert.AreEqual("updated", createdResult.RouteValues.ElementAt(0).Key);
+            Assert.AreEqual(true, createdResult.RouteValues.ElementAt(0).Value);
+
+        }
+
+        private Lote GenerarLote()
+        {
+            return new Lote
+            {
+                Id = 1,
+                Creacion = DateTime.Today,
+                //VIN = Guid.NewGuid()
+                //Usuario = new Usuario(),
+                //Danio = new Danio()
+            };
+        }
+
+        private IEnumerable<Lote> GenerarLotees()
+        {
+            return new List<Lote>
+            {
+                new Lote
+                {
+                    Id = 2,
+                    Creacion = DateTime.Today,
+                    //Danio = new Danio(),
+                    //Usuario = new Usuario(),
+                    //VIN = Guid.NewGuid()
+                },
+                 new Lote
+                {
+                    Id = 3,
+                    Creacion = DateTime.Today,
+                    //Danio = new Danio(),
+                    //Usuario = new Usuario(),
+                    //VIN = Guid.NewGuid()
+                }
+            };
+
+        }
     }
 }
---
+++ LogiCAR.Entidades.Test/LoteTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogiCAR.Entidades.Test
+{
+    [TestClass]
+    public class LoteTest
+    {
+        [TestMethod]
+        public void CrearLote()
+        {
+            Lote lote = new Lote();
+            lote.Creador = new Usuario();
+            lote.Nombre = "Lote 1";
+            lote.Decripcion = "Lote urgente";
+            lote.ProntoParaPartida = false;
+            lote.Vehiculos = new List<Vehiculo>();
+            Assert.IsNotNull(lote);
+
+        }
+    }
+}
