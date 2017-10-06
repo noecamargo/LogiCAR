f4ba7f85e698315676182e428949b7d53b1bc910
acapece <acapece@velocitypartners.net>
2017-10-05 23:41:51 -0300

    Creacion Entidad Lote

---
+++ LogiCAR.Entidades/Lote.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace LogiCAR.Entidades.Test
+{
+    public class Lote
+    {
+        [Key]
+        public long Id { get; set; }
+
+        public Usuario Creador { get; set; }
+        public string Decripcion { get; set; }
+        public string Nombre { get; set; }
+        public bool ProntoParaPartida { get; set; }
+        public List<Vehiculo> Vehiculos { get; set; }
+    }
+}
