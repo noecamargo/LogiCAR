ac02ff7a7ade9dea1e28934905056f39af46a846
acapece <acapece@velocitypartners.net>
2017-10-06 17:12:50 -0300

    Creacion prueba transporteLote

---
+++ Logi.CAR.WebApi.Tests/TransporteLote.cs
@@ -0,0 +1,32 @@
+﻿using LogiCAR.CapaLogicaNegocio;
+using LogiCAR.Entidades;
+using LogiCAR.WebApi.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace Logi.CAR.WebApi.Tests
+{
+    [TestClass]
+    public class TransporteLoteControllerTest
+    {
+        [TestMethod]
+        public void CrearLoteController()
+        {
+            var lote = GenerarTransporteLote();
+
+           
+        }
+
+        private object GenerarTransporteLote()
+        {
+            TransporteLote transporte = new TransporteLote();
+        }
+    }
+}
