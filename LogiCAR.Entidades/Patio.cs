918288a91b3e1f3f3f39aa0800adab696cfe5531
acapece <acapece@velocitypartners.net>
2017-10-08 01:15:58 -0300

    Se sube clase patio fltante del merge de Lugares

---
+++ LogiCAR.Entidades/Patio.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LogiCAR.Entidades
+{
+    public class Patio : Lugar
+    {
+        
+        public virtual List<Vehiculo> vehiculos { get; set; }
+
+        public Patio()
+        {
+            vehiculos = new List<Vehiculo>();
+	    }
+    }
+}
